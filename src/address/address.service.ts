f528b054361bcfb99f86e9941dafc9c73656de90
VictorSantuccii <victorfregne@gmail.com>
2024-06-18 20:16:25 -0300

    feat: insert validation in address

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -3,17 +3,23 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
 import { Repository } from 'typeorm';
 import { createAddressDto } from './dtos/createAddress.dto';
+import { UserService } from 'src/user/user.service';
+import { CityService } from 'src/city/city.service';
 
 @Injectable()
 export class AddressService {
 
     constructor(
         @InjectRepository(AddressEntity)
-        private readonly addressRepository: Repository<AddressEntity>
+        private readonly addressRepository: Repository<AddressEntity>,
+        private readonly userService: UserService,
+        private readonly cityService: CityService
 
     ) {}
 
-    async createAddress(createAddress: createAddressDto, userId: number) {
+    async createAddress(createAddress: createAddressDto, userId: number): Promise<AddressEntity> {
+        const user = await this.userService.findUserById(userId)
+        const city = await this.cityService.findCityById(createAddress.cityId)
       return  this.addressRepository.save({
             ...createAddress,
             userId
